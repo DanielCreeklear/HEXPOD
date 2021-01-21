d8deba7f23a3d1645f55d9552233e559670d032c
Daniel Lopes <ddanielssoares@gmail.com>
2021-01-20 21:21:45 -0300

    Alteração para Ponte-H

--- main.ino
+++ main.ino
@@ -1,4 +1,4 @@
-/* ------------- MOVIMENTO PATAS VERSAO 2.3.5 ------------- 
+/* ------------- MOVIMENTO PATAS VERSAO 2.3.6 ------------- 
  *  Desenvolvedores: Daniel Lopes / Enrique Emanuel
  *  ETEC Martin Luther King
  *  Sao Paulo(SP), Brasil - 2019
@@ -10,34 +10,36 @@
  *  
  *  =========Log de atualizacoes=============
  *  CONTEUDO NOVO:
- *  - Portas para controlar o LED e a Camera.
- *  - Acrescentado funcoes aos botoes S, Z, C, G e B sendo:
- *  S: Movimentacao autonoma
- *  G: Liga/Desliga LED
- *  B: Liga/Desliga Camera
- *  - Criada funcao test(), que executa apenas o front(). 
- *  - Criada funcao controle(), que executa todo o funcionamento do robo atraves do controle.
+ *  - Portas Ponte H
  * CONTEUDO MODIFICADO: 
- * - Valor da constante 'delay1' mudado para 650
+ * - Controle dos servos trocado pela Ponte H
  * CONTEUDO EXCLUIDO:
- * - Biblioteca Software Serial e todas suas linhas de comandos
- * - Função bateria()
+ * - Qualquer coisa envolvida com os servos
  * 
  */
 //Bibliotecas
 #include <VirtualWire.h>
 #include <VirtualWire_Config.h>
-#include <Wire.h>
-#include <Adafruit_PWMServoDriver.h>
 
-
-
-Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x40);  //Seleciona o shield com endereco 0x40
-
-//Configuração PWM para o servo motor.
-#define SERVOMIN  150  //Comprimento de pulso minimo
-#define SERVOMAX  600  //Comprimento de pulso maximo
-
+//Porta das Pontes H
+  //Motor 1
+  #define IN1  22
+  #define IN2  23
+  //Motor 2
+  #define IN3  24
+  #define IN4  25
+  //Motor 3
+  #define IN5  26
+  #define IN6  27
+  //Motor 4
+  #define IN7  28
+  #define IN8  29
+  //Motor 5
+  #define IN9  30
+  #define IN10 31
+  //Motor 6
+  #define IN11 32
+  #define IN12 33
 //Porta dos Sensores
 uint8_t motores[6] = {2, 3, 4, 5, 6, 7 }; //array que define os pinos dos motores
 #define sensorA 2
@@ -47,7 +49,6 @@ uint8_t motores[6] = {2, 3, 4, 5, 6, 7 }; //array que define os pinos dos motore
 #define sensorE 6
 #define sensorF 7
 #define pinRF  12
-#define pinLED 13
 boolean LED = 0;
 boolean CAMERA = 0;
 struct tipoPacote {
@@ -56,20 +57,11 @@ struct tipoPacote {
 tipoPacote pacote; 
 uint8_t buf[sizeof(pacote)];
 uint8_t buflen = sizeof(pacote);
+
 //Delays para movimento das patas
-  //4V:
   #define delay1 650 //Delay para iniciar o lado B
   #define delay2 100 //Delay para parar o lado A
-  //5V:
-  #define delay3 900 //Delay para iniciar o lado B
-  #define delay4 100 //Delay para parar o lado A
-  //6V:
-  #define delay5 700 //Delay para iniciar o lado B
-  #define delay6 100 //Delay para parar o lado A
-  //7V:
-  #define delay7 600 //Delay para iniciar o lado B
-  #define delay8 100 //Delay para parar o lado A
-
+  
 //Declarando Funções que serão utilizadas:
 void front_auto();
 int  graus(int x);
@@ -81,9 +73,8 @@ void controle();
 void test();
 
 void setup() {
-  pwm.begin(); //Inicia o controle dos servos(PWM)
-  pwm.setPWMFreq(50); //Frequencia de comunicaçao com o driver em 50Hz
 
+  for(int i = 22; i<34; i++) pinMode(i , OUTPUT);//Portas para controlar Ponte H
   for(int i = 0; i<7; i++) pinMode(motores[i] , INPUT_PULLUP); //Portas 2 - 8 como entradas com resistor Pullup
   pinMode(8, OUTPUT);
   pinMode(9, OUTPUT);
@@ -91,6 +82,7 @@ void setup() {
   vw_set_rx_pin(pinRF);
   vw_setup(2000);   
   vw_rx_start();
+  
 }
 
 void loop() {
@@ -139,66 +131,135 @@ void test(){
   front();
 }
 void front(){     //Funcao que faz o Hexpod andar para frente
-  pwm.setPWM(0, 0, graus(180));     //Motor 0 gira com sentido para frente no robo 
-  pwm.setPWM(4, 0, graus(0));       //Motor 4(motor oposto) gira com sentido para frente no robo
-  pwm.setPWM(2, 0, graus(180));     //Motor 2 gira com sentido para frente no robo
-  delay(delay1);                       //Delay para iniciar o lado B
-  pwm.setPWM(3, 0, graus(0));       //Motor 3 gira com sentido para frente no robo
-  pwm.setPWM(1, 0, graus(180));     //Motor 1(motor oposto) gira com sentido para frente no robo
-  pwm.setPWM(5, 0, graus(0));       //Motor 5 gira com sentido para frente no robo
-  delay(delay2);                       //Delay para parar o lado A
-  pwm.setPWM(0, 0, graus(51));      //Motor 0 parado
-  pwm.setPWM(4, 0, graus(56));      //Motor 4 parado
-  pwm.setPWM(2, 0, graus(51));      //Motor 2 parado
-  int a = delay1 - delay2;
-  delay(a);                       //Delay para parar o lado B
-  pwm.setPWM(3, 0, graus(51));      //Motor 3 parado
-  pwm.setPWM(1, 0, graus(51));      //Motor 1 parado
-  pwm.setPWM(5, 0, graus(51));      //Motor 5 parado
-  //delay(10);                        //Delay apenas para diminuir a velocidade do processamento
-  parado(0);                     //Chama a funaao parado que sincroniza as patas ao chao, com sentido para frente do robo
+  //Motor 1 Frente robo:
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  //Motor 5(oposto) Frente robo:
+  digitalWrite(IN10, HIGH);
+  digitalWrite(IN9, LOW);
+  //Motor 3 Frente robo:
+  digitalWrite(IN5, HIGH);
+  digitalWrite(IN6, LOW);
+    delay(delay1);//Delay para iniciar o lado B
+  //Motor 4 Frente robo:
+  digitalWrite(IN7, HIGH);
+  digitalWrite(IN8, LOW);
+  //Motor 2 Frente robo:
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  //Motor 6 Frente robo:
+  digitalWrite(IN11, HIGH);
+  digitalWrite(IN12, LOW);
+    delay(delay2);//Delay para parar o lado A
+  //Motor 1 Frente robo:
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, HIGH);
+  //Motor 5 Frente robo:
+  digitalWrite(IN9, HIGH);
+  digitalWrite(IN10,HIGH);
+  //Motor 3 Frente robo:
+  digitalWrite(IN5, HIGH);
+  digitalWrite(IN6, HIGH);
+    int a = delay1 - delay2;
+    delay(a);//Delay para parar o lado B
+  //Motor 4 Frente robo:
+  digitalWrite(IN7, HIGH);
+  digitalWrite(IN8, HIGH);
+  //Motor 2 Frente robo:
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, HIGH);
+  //Motor 6 Frente robo:
+  digitalWrite(IN11, HIGH);
+  digitalWrite(IN12, HIGH);
+    parado(0);//Chama a funaao parado que sincroniza as patas ao chao, com sentido para frente do robo
 }
 
 void right(){
-  pwm.setPWM(0, 0, graus(180));     //Motor 0 gira com sentido para frente no robo 
-  pwm.setPWM(4, 0, graus(0));       //Motor 4(motor oposto) gira com sentido para frente no robo
-  pwm.setPWM(2, 0, graus(180));     //Motor 2 gira com sentido para frente no robo
-  delay(delay1);                       //Delay para iniciar o lado B
-  pwm.setPWM(3, 0, graus(180));       //Motor 3 gira com sentido para frente no robo
-  pwm.setPWM(1, 0, graus(0));     //Motor 1(motor oposto) gira com sentido para frente no robo
-  pwm.setPWM(5, 0, graus(180));       //Motor 5 gira com sentido para frente no robo
-  delay(delay2);                       //Delay para parar o lado A
-  pwm.setPWM(0, 0, graus(51));      //Motor 0 parado
-  pwm.setPWM(4, 0, graus(51));      //Motor 4 parado
-  pwm.setPWM(2, 0, graus(51));      //Motor 2 parado
-  int a = delay1 - delay2;
-  delay(a);   
-  pwm.setPWM(3, 0, graus(51));      //Motor 3 parado
-  pwm.setPWM(1, 0, graus(51));      //Motor 1 parado
-  pwm.setPWM(5, 0, graus(51));      //Motor 5 parado
-  //delay(10);                        //Delay apenas para diminuir a velocidade do processamento
-  parado(1);                        //Chama a funaao parado que sincroniza as patas ao chao, com sentido para direita do robo
+  //Motor 1 Frente robo:
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  //Motor 5(oposto) Frente robo:
+  digitalWrite(IN10, HIGH);
+  digitalWrite(IN9, LOW);
+  //Motor 3 Frente robo:
+  digitalWrite(IN5, HIGH);
+  digitalWrite(IN6, LOW);
+    delay(delay1);//Delay para iniciar o lado B
+  //Motor 4 Frente robo:
+  digitalWrite(IN7, LOW);
+  digitalWrite(IN8, HIGH);
+  //Motor 2 Frente robo:
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+  //Motor 6 Frente robo:
+  digitalWrite(IN11, LOW);
+  digitalWrite(IN12, HIGH);
+    delay(delay2);//Delay para parar o lado A
+  //Motor 1 Frente robo:
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, HIGH);
+  //Motor 5 Frente robo:
+  digitalWrite(IN9, HIGH);
+  digitalWrite(IN10,HIGH);
+  //Motor 3 Frente robo:
+  digitalWrite(IN5, HIGH);
+  digitalWrite(IN6, HIGH);
+    int a = delay1 - delay2;
+    delay(a);//Delay para parar o lado B
+  //Motor 4 Frente robo:
+  digitalWrite(IN7, HIGH);
+  digitalWrite(IN8, HIGH);
+  //Motor 2 Frente robo:
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, HIGH);
+  //Motor 6 Frente robo:
+  digitalWrite(IN11, HIGH);
+  digitalWrite(IN12, HIGH);
+  parado(0);                        //Chama a funaao parado que sincroniza as patas ao chao, com sentido para direita do robo
 }
 
-void left(){     //Funcao que faz o Hexpod andar para frente
-  pwm.setPWM(0, 0, graus(0));     //Motor 0 gira com sentido para frente no robo 
-  pwm.setPWM(4, 0, graus(180));       //Motor 4(motor oposto) gira com sentido para frente no robo
-  pwm.setPWM(2, 0, graus(0));     //Motor 2 gira com sentido para frente no robo
-  delay(delay1);                       //Delay para iniciar o lado B
-  pwm.setPWM(3, 0, graus(0));       //Motor 3 gira com sentido para frente no robo
-  pwm.setPWM(1, 0, graus(180));     //Motor 1(motor oposto) gira com sentido para frente no robo
-  pwm.setPWM(5, 0, graus(0));       //Motor 5 gira com sentido para frente no robo
-  delay(delay2);                       //Delay para parar o lado A
-  pwm.setPWM(0, 0, graus(51));      //Motor 0 parado
-  pwm.setPWM(4, 0, graus(51));      //Motor 4 parado
-  pwm.setPWM(2, 0, graus(51));      //Motor 2 parado
-  int a = delay1 - delay2;
-  delay(a);   
-  pwm.setPWM(3, 0, graus(51));      //Motor 3 parado
-  pwm.setPWM(1, 0, graus(51));      //Motor 1 parado
-  pwm.setPWM(5, 0, graus(51));      //Motor 5 parado
-  //delay(10);                        //Delay apenas para diminuir a velocidade do processamento
-  parado(2);                     //Chama a funaao parado que sincroniza as patas ao chao, com sentido para esquerda do robo
+void left(){     //Funcao que faz o Hexpod andar para a esquerda
+  //Motor 1 Frente robo:
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  //Motor 5(oposto) Frente robo:
+  digitalWrite(IN10,LOW);
+  digitalWrite(IN9, HIGH);
+  //Motor 3 Frente robo:
+  digitalWrite(IN5, LOW);
+  digitalWrite(IN6, HIGH);
+    delay(delay1);//Delay para iniciar o lado B
+  //Motor 4 Frente robo:
+  digitalWrite(IN7, HIGH);
+  digitalWrite(IN8, LOW);
+  //Motor 2 Frente robo:
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  //Motor 6 Frente robo:
+  digitalWrite(IN11, HIGH);
+  digitalWrite(IN12, LOW);
+    delay(delay2);//Delay para parar o lado A
+  //Motor 1 parado:
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, HIGH);
+  //Motor 5 parado:
+  digitalWrite(IN9, HIGH);
+  digitalWrite(IN10,HIGH);
+  //Motor 3 parado:
+  digitalWrite(IN5, HIGH);
+  digitalWrite(IN6, HIGH);
+    int a = delay1 - delay2;
+    delay(a);//Delay para parar o lado B
+  //Motor 4 parado:
+  digitalWrite(IN7, HIGH);
+  digitalWrite(IN8, HIGH);
+  //Motor 2 parado:
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, HIGH);
+  //Motor 6 parado:
+  digitalWrite(IN11, HIGH);
+  digitalWrite(IN12, HIGH);
+  parado(0); //Chama a funcao parado que sincroniza as patas ao chao, com sentido para esquerda do robo
 }
 
 void parado(int sentido){       //Funcao que sincroniza as patas no chao, recebe valor: sentido de rotacao, para sincronizar
@@ -207,55 +268,61 @@ void parado(int sentido){       //Funcao que sincroniza as patas no chao, recebe
 
       //Sincronizar Motor 2:  
       if(!digitalRead(sensorB)){    //Se o sensor B, localizado na porta 3, estiver ativado
-        pwm.setPWM(1, 0, graus(51));//Motor 1 parado
+        digitalWrite(IN1, HIGH);
+        digitalWrite(IN2, HIGH);
       } else {                      //Se não
           do{                       //Executa uma vez o que estiver dentro de "do"
-            pwm.setPWM(1, 0, graus(180));//Motor 1 gira com sentido para frente no robo 
+            digitalWrite(IN1, HIGH);
+            digitalWrite(IN2, LOW);
           }while(digitalRead(sensorB));//Enquanto sensor B estiver desativado ele executa "do"
           if(!digitalRead(sensorB)){//Se o sensor B, localizado na porta 3, estiver ativado
-            pwm.setPWM(1, 0, graus(51));//Motor 1 parado
+            digitalWrite(IN1, HIGH);
+            digitalWrite(IN2, HIGH);
           }                         //
       }                             //
 
-
-
       //Sincronizar Motor 5:
       if(!digitalRead(sensorE)){
-        pwm.setPWM(4, 0, graus(56));
+        digitalWrite(IN9, HIGH);
+        digitalWrite(IN10,HIGH);
       } else {
           do{
-            pwm.setPWM(4, 0, graus(0));
+            digitalWrite(IN9, HIGH);
+            digitalWrite(IN10,LOW);
           } while(digitalRead(sensorE));
           if(!digitalRead(sensorE)){
-            pwm.setPWM(4, 0, graus(56));
+            digitalWrite(IN9, HIGH);
+            digitalWrite(IN10,HIGH);
           }
       }
-
-
 
       //Sincronizar Motor 6:
       if(!digitalRead(sensorF)){
-        pwm.setPWM(5, 0, graus(51));
+        digitalWrite(IN11, HIGH);
+        digitalWrite(IN12, HIGH);
       } else {
           do{
-            pwm.setPWM(5, 0, graus(0));
+            digitalWrite(IN11, HIGH);
+            digitalWrite(IN12, LOW);
           }while(digitalRead(sensorF));
           if(!digitalRead(sensorF)){
-            pwm.setPWM(5, 0, graus(51));
+            digitalWrite(IN11, HIGH);
+            digitalWrite(IN12, HIGH);
           }
       }
-
-
-
+      
       //Sincronizar Motor 3:
       if(!digitalRead(sensorC)){    //Se o sensor C, localizado na porta 4, estiver ativado
-        pwm.setPWM(2, 0, graus(51));//Motor 2 parado
+        digitalWrite(IN5, HIGH);
+        digitalWrite(IN6, HIGH);
       } else {                      //Se não
           do{                       //Executa uma vez o que estiver dentro de "do"
-            pwm.setPWM(2, 0, graus(180));//Motor 2 gira com sentido para frente no robo
+            digitalWrite(IN5, HIGH);
+            digitalWrite(IN6, LOW);
           }while(digitalRead(sensorC));//Enquanto sensor B estiver desativado ele executa "do"
           if(!digitalRead(sensorC)){//Se o sensor C, localizado na porta 4, estiver ativado
-            pwm.setPWM(2, 0, graus(51));//Motor 2 parado
+            digitalWrite(IN5, HIGH);
+            digitalWrite(IN6, HIGH);
           }                         //
       }                             //
 
@@ -263,13 +330,16 @@ void parado(int sentido){       //Funcao que sincroniza as patas no chao, recebe
       
     //Sincronizar Motor 1:
       if(!digitalRead(sensorA)){    //Se o sensor A, localizado na porta 2, estiver ativado
-        pwm.setPWM(0, 0, graus(51));//Motor 0 parado
+        digitalWrite(IN1, HIGH);
+        digitalWrite(IN2, HIGH);
       } else {                      //Se não
           do {                      //Executa uma vez o que estiver dentro de "do"
-            pwm.setPWM(0, 0, graus(180));//Motor 0 gira com sentido para frente no robo 
+            digitalWrite(IN1, HIGH);
+            digitalWrite(IN2, LOW); 
           } while(digitalRead(sensorA));//Enquanto sensor A estiver desativado ele executa "do"
           if(!digitalRead(sensorA)){//Se o sensor A, localizado na porta 2, estiver ativado
-            pwm.setPWM(0, 0, graus(51));//Motor 0 parado
+            digitalWrite(IN1, HIGH);
+            digitalWrite(IN2, HIGH);
           }                         //
       }                             //
 
@@ -277,16 +347,20 @@ void parado(int sentido){       //Funcao que sincroniza as patas no chao, recebe
       
     //Sincronizar Motor 4:               
       if(!digitalRead(sensorD)){
-        pwm.setPWM(3, 0, graus(51));
+        digitalWrite(IN7, HIGH);
+        digitalWrite(IN8, HIGH);
       } else {
           do{
-            pwm.setPWM(3, 0, graus(0));
+            digitalWrite(IN7, HIGH);
+            digitalWrite(IN8, LOW);
           }while(digitalRead(sensorD));
           if(!digitalRead(sensorD)){
-            pwm.setPWM(3, 0, graus(51));
+            digitalWrite(IN7, HIGH);
+            digitalWrite(IN8, LOW);
           }
       }
   }//end if
+  /*
   if(sentido == 1) {                    //Se sentido for 1, sincroniza as patas para direita do robo
 
 
@@ -453,14 +527,17 @@ void parado(int sentido){       //Funcao que sincroniza as patas no chao, recebe
           }
       }
   }//end if
+  */
 }//end void parado
-  
+
+/*
 int graus(int x) { //Funçao 'graus' que recebe um valor Inteiro armazenado em 'x'                                                            
   int graus = x; //Variavel inteira 'graus' recebe o valor de 'x'
   graus = map(graus, 0, 180, SERVOMIN, SERVOMAX); //Regra de 3, para converter graus no valor do PWM
   
   return graus; //Retorna como resultado da funcao, o valor da variavel 'graus'
 }
+*/
 
 void front_auto(){
   
